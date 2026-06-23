6a818fc309868e16c76ab4f88dd3b60de6893c62
diane1234 <dianekimlim@gmail.com>
2026-06-22 21:54:55 -0400

    syntax updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sidequest Journal';
-export const SITE_DESCRIPTION = 'Notes, detours, and things worth remembering.';
+export const SITE_TITLE = 'sidequest journal';
+export const SITE_DESCRIPTION = 'notes, detours, and things worth remembering.';
